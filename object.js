4e24294f18655c5085bd77f57524e4d99edc6c81
Imran <imran.bursagov@gmail.com>
2022-05-18 10:39:09 +0300

    imran

---
+++ object.js
@@ -0,0 +1,13 @@
+const soldier = {
+  name: 'Masterchief',
+  health: 10,
+  ammo: {
+    gunName: 'ПП',
+    bullets: 30,
+  },
+  supply: 3,
+
+  fire: function () {},
+  reload: function () {},
+  wound: function () {},
+}
